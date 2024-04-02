171fa3b424cd6b7e9dcedc57809e5ab26c956281
testyahomepl <testyahomepl@wp.pl>
2024-04-03 00:52:50 +0200

    zajecia02

---
+++ tests/basicOperations.ts
@@ -0,0 +1,10 @@
+// console.log('hello word')
+
+// let number2: number = 10;
+// let string1: string = "30"
+// let sum = number2 + string1
+// console.log(sum);
+
+let auto ={"marka":"fiat","kolor":"czerwony"}
+
+console.log(auto.kolor)
