b2019a890b49370fd73886650d3319082ca410bd
testyahomepl <testyahomepl@wp.pl>
2024-04-03 23:57:37 +0200

    trzecie zajęcia

--- playwright.config.ts
+++ playwright.config.ts
@@ -21,13 +21,18 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
+  timeout: 30000,
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
+    video: {
+      mode: "on",
+      size: {width: 1280, height: 720}
+    },
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'on',
   },
 
   /* Configure projects for major browsers */
@@ -37,15 +42,15 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
 
     /* Test against mobile viewports. */
     // {
---
+++ tests/basicLoops.ts
@@ -0,0 +1,13 @@
+
+const x:number = 4
+
+if (x > 5) {
+    console.log("liczba jest większa od 5")
+} 
+else {
+    console.log("liczba jest mniejsza od 5")
+}
+
+
+
+
